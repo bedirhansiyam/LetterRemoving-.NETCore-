0ec6a0660cc09737a3ba6b01b5656afeb00579dc
Bedirhan <bedirhansiyam@gmail.com>
2023-03-20 17:25:58 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LetterRemoving;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter a word and number (word,number) : ");
+
+        string[] input = Console.ReadLine().Split(',');
+        string word = input[0];
+        int number = Convert.ToInt32(input[1]);
+
+        if(number > word.Length-1)
+        {
+            Console.WriteLine("Result : " + word);
+        }
+        else
+            Console.WriteLine("Result : " + word.Remove(number,1));
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Letter Removing - .Net Core
+In this application, the user enters a word and a number with commas between and the application removes the character in the given index from the word and prints it on the console.</br>
+*Sample input: Mauritius,2 - Mauritius,0 - Mauritius,16* </br>
+*Sample output: Maritius - auritius - Mauritius*
+
+</br>
+
+[Patika.dev](https://app.patika.dev/)
